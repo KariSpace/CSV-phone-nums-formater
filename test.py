4aabab6e48250f173410ddfb9a3292b2d7e37203
csd-oss <mk@buialo.com>
2020-07-24 23:48:53 +0300

    created basic test that is failing for now

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 from csvConverter import main
+import pandas as pd
 
 main('test.csv')
+
+ok = pd.read_csv('ok_file.csv', sep=";")
+phones = ok['phone'].unique()
+if len(phones) == 1:
+    print('Vsye zaebis\'')
+else:
+    print('Hueta, blya ' * 3)
